b52d1ba44eea073cc7e908757de34356fdb51888
marcos-sco <marcos_sco@outlook.com>
2025-04-05 14:35:39 -0300

    feat: html items inclusion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,17 @@
 </head>
 
 <body>
-  <main class="main-container">
+  <header class="page-header py-5 text-white">
+
+    <h1 class="text-center">{{ __('Alfasoft Contacts') }}</h1>
+
+    <nav class="text-center pt-3 pb-2">
+      <a href="{{ route('contacts.index') }}" class="btn btn-light me-2 py-2 px-4">{{ __('List Contacts') }}</a>
+      <a href="{{ route('contacts.create') }}" class="btn btn-light py-2 px-4">+{{ __('Add Contact') }}</a>
+    </nav>
+  </header>
+
+  <main class="main-container container mt-4 mb-5">
 
     @yield('content')
   </main>
