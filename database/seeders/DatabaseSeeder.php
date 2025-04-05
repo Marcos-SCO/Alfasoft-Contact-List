b606f962fb51a00cba6ed36429ae20e528e4c1e5
marcos-sco <marcos_sco@outlook.com>
2025-04-05 13:40:52 -0300

    feat: contact seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +18,16 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::firstOrCreate(
+            ['email' => 'admin@test.com'],
+            ['name' => 'Alfasoft', 'password' => Hash::make('password')]
+        );
+        
+        User::firstOrCreate(
+            ['email' => 'marcos@test.com'],
+            ['name' => 'Marcos', 'password' => Hash::make('password')]
+        );
+
+        Contact::factory()->count(20)->create();
     }
 }
